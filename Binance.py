b39d30ca798fe4e49eaeec2482e8ecd2a5d364ca
Art Vandelay <goldman@uoguelph.ca>
2018-01-23 06:48:02 -0500

    Improved BinanceBidOrder infastructure. Added bidding simulation

--- Binance.py
+++ Binance.py
@@ -6,7 +6,7 @@ from Order import Order
 
 
 AVG_BID_INCREASE = .025
-CACHE_LIFESPAN = 10# seconds
+CACHE_LIFESPAN = .9# seconds
 TRADE_TAX = .001 #Binance's .1% fee
 
 
@@ -30,6 +30,12 @@ class DataCache:
 
     symbolInfo = {}
     lastSymbolUpdate = {}
+
+    ordersInfo = {}
+    lastOrdersInfo = {}
+
+    recentTrades = {}
+    lastTradesUpdate = {}
 
     dayTickerData = None
     lastDayTickerUpdate = 0
@@ -85,7 +91,7 @@ def order_abort(pair, orderId):
 def get_orderbook_tickers():
     if time.time() - DataCache.lastOrderBookUpdate >= CACHE_LIFESPAN:
         #fetch new
-        print("GETTING FRESH ORDERBOOK")
+       # print("GETTING FRESH ORDERBOOK")
         tickers = client.get_orderbook_tickers()
         DataCache.orderBookData = tickers
         DataCache.lastOrderBookUpdate = time.time()
@@ -97,7 +103,7 @@ def get_orderbook_tickers():
 def get_24hr_ticker():
     if time.time() - DataCache.lastDayTickerUpdate >= CACHE_LIFESPAN:
         #fetch new
-        print("GETTING FRESH 24HR")
+        #print("GETTING FRESH 24HR")
         ticker = client.get_ticker()
         DataCache.dayTickerData = ticker
         DataCache.lastDayTickerUpdate = time.time()
@@ -113,7 +119,7 @@ def get_symbol_data(symbol):
         DataCache.lastSymbolUpdate[symbol] = 0
 
     if time.time() - DataCache.lastSymbolUpdate[symbol] >= CACHE_LIFESPAN:
-        print("GETTING FRESH SYMBOL DATA")
+        #print("GETTING FRESH SYMBOL DATA")
         info = client.get_symbol_info(symbol)
         DataCache.symbolInfo[symbol] = info
         DataCache.lastSymbolUpdate[symbol] = time.time()
@@ -121,6 +127,26 @@ def get_symbol_data(symbol):
         info = DataCache.symbolInfo[symbol]
 
     return info
+
+
+def get_recent_trades(pair):
+    if pair not in DataCache.recentTrades:
+        DataCache.recentTrades[pair] = None
+        DataCache.lastTradesUpdate[pair] = 0
+
+    if time.time() - DataCache.lastTradesUpdate[pair] >= CACHE_LIFESPAN:
+        #print("GETTING FRESH RECENT TRADES")
+        info = client.get_recent_trades(symbol=pair)
+        DataCache.recentTrades[pair] = info
+        DataCache.lastTradesUpdate[pair] = time.time()
+    else:
+        info = DataCache.recentTrades[pair]
+
+    return info[::-1]
+
+
+
+
 
 def get_pair_volume(pair):
     ticker = get_24hr_ticker()
@@ -146,6 +172,39 @@ def get_price(pair):
         if p['symbol'] == pair:
             return Decimal(p['price'])
 
+def get_orders_data(pair):
+    if pair not in DataCache.ordersInfo:
+        DataCache.ordersInfo[pair] = None
+        DataCache.lastOrdersInfo[pair] = 0
+
+    if time.time() - DataCache.lastOrdersInfo[pair] >= CACHE_LIFESPAN:
+        #print("GETTING FRESH ORDERS DATA")
+        info = client.get_order_book(symbol=pair)
+        DataCache.ordersInfo[pair] = info
+        DataCache.lastOrdersInfo[pair] = time.time()
+    else:
+        info = DataCache.ordersInfo[pair]
+
+    return info
+
+
+def get_list_of_bids(pair):
+    data = get_orders_data(pair)
+
+    bids = []
+    for d in data['bids']:
+        bids.append((d[0], d[1]))
+
+    return bids
+
+def get_list_of_asks(pair):
+    data = get_orders_data(pair)
+
+    asks = []
+    for d in data['asks']:
+        asks.append((d[0], d[1]))
+
+    return asks
 
 #------------Account Info
 def get_current_balance(symbol):
@@ -269,13 +328,16 @@ class Symbol:
         price = self.get_price()
         return Decimal(spread/price)
 
-
     def buy(self, quantity, price):
         return order_buy(self.pairSymbol, quantity, price)
 
     def buy_max(self, price, amountToSpend):
         quantity = self.get_max_buyable_quant(price, amountToSpend)
-        return self.buy(quantity, price)
+        return self.buy(quantity, price), quantity
+
+    def sim_buy_max(self, price, amountToSpend):
+        quantity = self.get_max_buyable_quant(price, amountToSpend)
+        return '1111', quantity
 
     def sell(self, quantity, price):
         return order_sell(self.pairSymbol, quantity, price)
@@ -303,7 +365,6 @@ class Symbol:
 class BinanceBidOrder:
 
     sym = None
-    type = "BID"
     orderId = None
 
     bid = 0
@@ -311,6 +372,17 @@ class BinanceBidOrder:
     amountSpent = 0
 
     prepared = False
+    simulating = False
+
+    standAlone = False
+
+    quantCurrentlyOrdered = 0
+    quantBoughtTotal = 0
+    quantBoughtPartial = 0
+
+
+
+    lastTradeID = 0
 
 
     def __init__(self, symbol, amountToSpend):
@@ -321,27 +393,142 @@ class BinanceBidOrder:
     def prepare(self):
         if not self.prepared:
             bestBid = self.sym.get_best_bid()
-            self.bid = self.sym.increment_bid(bestBid)
+            #self.bid = self.sym.increment_bid(bestBid)
+            self.bid = bestBid
             self.prepared = True
         else:
             print("Trying to prepare an already prepared order")
 
+    def prepare_sim(self):
+        self.simulating = True
+        self.prepare()
 
     def place(self):
         if not self.prepared:
             print("Trying to place an unprepared order. .Prepare() the order first.")
             return
 
-        self.orderId = self.sym.buy_max(self.bid, self.amountToSpend)
+        if not self.simulating:
+            self.orderId, quant = self.sym.buy_max(self.bid, self.amountToSpend - self.amountSpent)
+        else:
+            self.orderId, quant = self.sym.sim_buy_max(self.bid, self.amountToSpend - self.amountSpent)
+
+
+        prev = self.quantCurrentlyOrdered
+        self.quantCurrentlyOrdered = quant
+        self.quantBoughtPartial = 0
+
+        if prev != self.quantCurrentlyOrdered:
+            print("Placed order for {} {}".format(self.quantCurrentlyOrdered, self.sym.minorSymbol))
+
+
 
 
     def abort(self):
+        if self.simulating:
+            self.orderId = None
+            self.bid = 0
+            self.amountToSpend = 0
+            self.amountSpent = 0
+            return
+
         if self.orderId:
             self.sym.abort_order(self.orderId)
         else:
             print("Trying to abort without an active order. Prepare and Place an order first")
 
 
+
+
+
+    def update(self):
+
+
+        #Up here would be soemthing like
+        #    orderFilled = __get_offer_state(offerId)
+        #   if offerFilled then return True
+
+     #   if not self.orderId:
+     #       print("Trying to improve a bid without having an active order")
+     #       return
+
+        if self.simulating:
+            trades = get_recent_trades(self.sym.pairSymbol)
+            if self.lastTradeID == 0:
+                self.lastTradeID = trades[0]['id']
+            #print(trades)
+            for t in trades:
+                if t['id'] == self.lastTradeID:
+                    self.lastTradeID = trades[0]['id']
+                    break
+
+                print("NEW TRADE: {}  MY BID: {}".format(t['price'], str(self.bid)))
+                if t['price'] == str(self.bid):
+                    print("-----------------------WE'VE MADE A SALE!!!")
+                    saleQuant = Decimal(t['qty'])
+
+                    if (self.quantCurrentlyOrdered - self.quantBoughtPartial) - saleQuant > 0:
+                        self.amountSpent += saleQuant * self.bid
+                        self.quantBoughtPartial += saleQuant
+                        self.quantBoughtTotal += saleQuant
+
+                        print("------PARITAL FILL: Bid: {} Total Bought: {} Partial Bought: {} ETH spent: {}".format(self.bid, self.quantBoughtTotal, self.quantBoughtPartial, self.amountSpent))
+                        self.lastTradeID = trades[0]['id']
+                        return False
+                    else:  # saleQuant - () <= 0:
+                        quantBought = self.quantCurrentlyOrdered - self.quantBoughtPartial
+                        self.amountSpent += quantBought * self.bid
+                        self.quantBoughtPartial += quantBought
+                        self.quantBoughtTotal += quantBought
+                        print("------COMPLETE FILL: Bid: {} Total Bought: {} Partial Bought: {} ETH spent: {}".format(self.bid, self.quantBoughtTotal, self.quantBoughtPartial, self.amountSpent))
+                        self.lastTradeID = trades[0]['id']
+                        return True
+            self.lastTradeID = trades[0]['id']
+
+
+
+
+
+
+
+
+
+        allBids = get_list_of_bids(self.sym.pairSymbol)
+        newBid = self.bid
+
+
+        for b in allBids:
+            nextBid = Decimal(b[0])
+
+            #print("{} -- {}".format(self.bid, b[0]))
+
+
+            if nextBid > self.bid:
+                if (nextBid - self.bid)/self.bid <= AVG_BID_INCREASE:
+                   # print("New Higher Bid: {}".format(nextBid))
+                    newBid = nextBid
+                    break
+            elif nextBid == self.bid:
+                if (self.quantCurrentlyOrdered - self.quantBoughtPartial == Decimal(b[1])):
+                    #print("Standing Alone")
+                    self.standAlone = True
+
+                else:
+                    #print("Not Alone")
+                    self.standAlone = False
+                    break
+            elif nextBid < self.bid:
+            #    if (self.bid - nextBid)/nextBid <= AVG_BID_INCREASE:
+                #print("We're Too High! New low bid: {}".format(nextBid))
+                newBid = nextBid
+                break
+
+        if newBid != self.bid:
+            print("Updating bid: {}".format(newBid) )
+        self.bid = newBid
+        self.place()
+
+        return False
 
 
 
@@ -369,8 +556,6 @@ def get_best_spread(symbol):
 
     maxSpread = 0
     bestPair = ""
-    bestAskPrice = 0
-    bestBidPrice = 0
     for s in data:
         pair = s['symbol']
         if symbol not in pair:
@@ -392,3 +577,15 @@ def get_best_spread(symbol):
 
     #bestSymData = get_symbol_data(bestPair)
     return bestPair, round(maxSpread,2) #Symbol(bestSymData)
+
+
+
+
+"""
+poop = get_recent_trades("BATETH")
+poop = poop[::-1]
+print(poop)
+
+for p in poop:
+    print(p)
+    """
